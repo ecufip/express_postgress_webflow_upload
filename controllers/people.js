8436a8c5d0e9f3be9779815d8b0cf381f1d43770
Sam Close <sclose86@gmail.com>
2017-12-08 12:30:42 +0000

    for loop de-nested

--- controllers/people.js
+++ controllers/people.js
@@ -23,40 +23,43 @@ exports.webflow_upload = function(req, res){
     
     var personArray = [];
     var resolvedPromiseArray = [];
+    var webflowObjectArray = [];
     
     // this determines how much of a collection to go through (e.g. 0-5 means first 500)
     for(var x = 0; x<5; x++){
         
         // limit cannot exceed 100 but offset can be used to 'ignore' a number of results at the beginning
-        const items = webflow.items({ collectionId: 'TODO' }, { limit: 100, offset: x * 100});
-
+        const items = webflow.items({ collectionId: 'TODO' }, { limit: 10, offset: x * 100});
+    
         // pushes promise to array
         resolvedPromiseArray.push(items);
+        
+        // builds an array of all of the Webflow objects
+        items.then(function(webflowObjects){
+            return webflowObjectArray.push.apply(webflowObjectArray, webflowObjects.items);
+        })
+    }
 
-        // gets the array of items, updates them to fit with the schema and then adds them to personArray
-        items.then(function(i){
-            var itemArray = i.items;
-            // to put items into 'schema friendly' format
-            var updatedItemArray = [];
-            itemArray.forEach(function(item) {
-                // creates array from Webflow items
-                updatedItem = {
-                                'name': item.name,
-                            }
-                updatedItemArray.push(updatedItem)
-            });
-            personArray.push.apply(personArray, updatedItemArray); 
-        });
-    } 
-    
-    // once all the promises in the resolvedPromiseArray have been resolved, add all to database
+    // once all the promises in the resolvedPromiseArray have been resolved, creates item array and add all to database
     Promise.all(resolvedPromiseArray).then(function(){
-        People.bulkCreate(personArray, {returning: true}).then(people => res.status(200).send(people))
-    });
+        return Promise.all(webflowObjectArray.map(function(item){
+            person = {
+                'name': item.name
+            }
+            return person
+        }));
+    })
+    .then(function(personArray){
+        // takes array of Sequelize friendly objects and puts them into database
+        People.bulkCreate(personArray, {returning: true}).then(people => res.status(200).send(people));
+    })
+    .catch(error => res.status(400).send(error)); 
 };
 
+
+
 exports.delete_all = function(req, res){
-    return Person
+    return People
     .destroy({
         where: {}
     })
